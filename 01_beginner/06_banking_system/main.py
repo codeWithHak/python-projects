f21a041821a02ff685edbfaaec19793774fccedc
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-06-18 18:35:56 +0500

    learn read, write,  binary and with in file handling

--- 01_beginner/06_banking_system/main.py
+++ 01_beginner/06_banking_system/main.py
@@ -1,4 +1,24 @@
+# read
 file = open("data.txt", "r")
 content = file.read()
 print(content)
 file.close()
+
+# read binary
+file = open("data.txt", "rb")
+content = file.read()
+print(content)
+file.close()
+
+# append and read
+file = open("data.txt", "a+")
+content = file.read()
+print(content)
+file.write("AAhhooo paajji")
+file.close()
+
+# open file with 'with'
+with open("data.txt", "r") as file:
+    content = file.read()
+    print(content)
+    file.close()
